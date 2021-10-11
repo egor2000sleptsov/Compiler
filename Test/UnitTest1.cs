9a390492589a34f1a1af745a6f50b73330ec1561
Egor <egor2000sleptsov@gmail.com>
2021-10-11 19:33:05 +1000

    11.10.2021

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+using Compiler;
+
+namespace Test {
+    public class Tests {
+        [SetUp]
+        public void Setup() { }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
